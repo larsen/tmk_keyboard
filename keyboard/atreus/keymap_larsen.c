24f58c7562d25904a33298b192efc5e388a64267
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-03-29 11:44:20 +0200

    more readable

--- keyboard/atreus/keymap_larsen.c
+++ keyboard/atreus/keymap_larsen.c
@@ -7,36 +7,39 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: mostly letters */
-  KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
-         KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
-         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,     \
-         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT),                     \
+  KEYMAP(
+    KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                        KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    \
+    KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                        KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, \
+    KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                        KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, \
+    KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT,  KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT   \
+  ),
+
   /* 1: punctuation and numbers */
   FN_ARROW_LAYER,                                     \
+
   /* 2: arrows and function keys */
   /*
    * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
    *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
+   *       volup  prev next  reset      ||             F1    F2    F3   F12
    *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
    */
-  KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, \
-         KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
-         KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, \
-         KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11,          \
-         KC_FN4, KC__VOLUP, KC_NO, KC_NO, KC_FN3, \
-         KC_NO, KC_F1, KC_F2, KC_F3, KC_F12,            \
-         KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, \
-         KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE),
+  KEYMAP(
+    KC_INS, KC_HOME,     KC_UP,   KC_END,   KC_PGUP,                     KC_UP,   KC_F7, KC_F8,       KC_F9,   KC_F10,  \
+    KC_DEL, KC_LEFT,     KC_DOWN, KC_RIGHT, KC_PGDN,                     KC_DOWN, KC_F4,  KC_F5,      KC_F6,   KC_F11,  \
+    KC_FN4, KC__VOLUP,   KC_MPRV, KC_MNXT,  KC_FN3,                      KC_NO,   KC_F1,  KC_F2,      KC_F3,   KC_F12,  \
+    KC_NO,  KC__VOLDOWN, KC_LGUI, KC_LSFT,  KC_BSPC,  KC_LCTL, KC_LALT,  KC_SPC,  KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE \
+  ),
+
   /* 3: mouse control, reached by fn+esc, then holding z
-         //       left    up/down  right                              mouse wheel: left up/down right
-         //       acceleration settings                               buttons:    left   middle   right
   */
-  KEYMAP(KC_NO,   KC_NO,   KC_FN6,  KC_NO,   KC_NO,                   KC_NO,   KC_NO,   KC_FN10, KC_NO,   KC_NO,   \
-         KC_NO,   KC_FN8,  KC_FN7,  KC_FN9,  KC_NO,                   KC_NO,   KC_FN12, KC_FN11, KC_FN13, KC_NO,   \
-         KC_NO,   KC_FN19, KC_FN20, KC_FN21, KC_NO,                   KC_FN17,   KC_FN14, KC_FN16, KC_FN15, KC_FN18, \
-         KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_NO,     KC_FN5,  KC_NO,   KC_NO,   KC_NO)
+  KEYMAP(
+    KC_NO,   KC_NO,   KC_FN6,  KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_FN10, KC_NO,   KC_NO,     \
+    //       left    up/down  right                                mouse wheel: left up/down right
+    KC_NO,   KC_FN8,  KC_FN7,  KC_FN9,  KC_NO,                     KC_NO,   KC_FN12, KC_FN11, KC_FN13, KC_NO,     \
+    //       acceleration settings                                 buttons:    left   middle   right
+    KC_NO,   KC_FN19, KC_FN20, KC_FN21, KC_NO,                     KC_FN17,   KC_FN14, KC_FN16, KC_FN15, KC_FN18, \
+    KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, KC_NO,  KC_TRNS, KC_TRNS,  KC_NO,     KC_FN5,  KC_NO,   KC_NO,   KC_NO)
 
 };
 
